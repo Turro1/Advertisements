7e317aca2ac493dcd88be62df529034c11a00876
Turro1 <87533348+Turro1@users.noreply.github.com>
2022-06-02 00:00:20 +0300

    Update ValidationBehavior.cs

--- Advertisements.Application/Common/Behaviors/ValidationBehavior.cs
+++ Advertisements.Application/Common/Behaviors/ValidationBehavior.cs
@@ -10,7 +10,7 @@ using FluentValidation;
 namespace Advertisements.Application.Common.Behaviors
 {
     public class ValidationBehavior
-    {<TRequest, TResponse>
+    <TRequest, TResponse>
       : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
@@ -33,7 +33,6 @@ namespace Advertisements.Application.Common.Behaviors
             }
             return next();
         }
-    }
     }
 }
 
