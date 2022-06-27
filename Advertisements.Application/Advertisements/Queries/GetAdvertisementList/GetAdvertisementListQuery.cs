0fcacf33103b76305a3621dff06833af7a112968
Turro1 <87533348+Turro1@users.noreply.github.com>
2022-06-27 09:52:18 +0300

    Add files via upload

---
+++ Advertisements.Application/Advertisements/Queries/GetAdvertisementList/GetAdvertisementListQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Advertisements.Application.Advertisements.Queries.GetAdvertisementList
+{
+    public class GetAdvertisementListQuery : IRequest<AdvertisementListVm>
+    {
+        public Guid UserId { get; set; }
+        public int? Number { get; set; }
+        public string? Text { get; set; }
+        public int Page { get; set; }
+    }
+}
